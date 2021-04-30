a187a3abc06591db4b227ce32bc5db6c7622fe46
ltanksley10 <contactlthere@gmail.com>
2021-04-30 13:48:18 +0000

    added more features and mobile screen responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import fetch from 'isomorphic-fetch';
+import moment from 'moment';
 
 const apiKey = process.env.REACT_APP_WEATHER_KEY;
 const baseURL = "https://api.openweathermap.org/data/2.5/";
 
-function App() {
+function App () {
   
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
@@ -18,8 +19,8 @@ function App() {
           setQuery('');
           console.log(result);
         });
-    }
-  }
+    } 
+  };
   
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August",
@@ -31,40 +32,52 @@ function App() {
     let month = months[d.getMonth()];
     let year = d.getFullYear();
     
-    return `${day} ${date} ${month} ${year}`
-  }
+    return `${day} ${date} ${month} ${year}`;
+  };
   
   return (
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 32) ? 'app warm'
-      : 'app') : 'app' }>
-      <main>
-        <div className="search-box">
-          <input 
-            type="text"
-            className="search-bar"
-            placeholder="Search... (City, State, Country)"
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
-        </div>
-        {(typeof weather.main != "undefined") ? (
-        <div>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
+    <div>
+      <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 50) ? 'app warm'
+        : 'app') : 'app' }>
+        <main>
+          <div className="search-box">
+            <h1>Current Weather Tracker</h1>
+            <input 
+              type="text"
+              className="search-bar"
+              placeholder="Search... (City, State, Country)"
+              onChange={e => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
+            />
           </div>
-          <div className="weather-box">
-            <div className="temp">
-              {Math.round(weather.main.temp)}&deg;f
+          {(typeof weather.main != "undefined") ? (
+          <div>
+            <div>
+              <div className="location-box">
+                <div className="location">{weather.name}, {weather.sys.country}</div>
+                <div className="date">{dateBuilder(new Date())}</div>
+              </div>
+              <div className="weather-box">
+                <div className="temp">
+                  {Math.round(weather.main.temp)}&deg;f
+                </div>
+                <div className="weather">{weather.weather[0].description}</div>
+                <div className="cloud">Cloudiness: {weather.clouds.all}%</div>
+                <div className="wind">Wind Speed: {Math.round(weather.wind.speed)} mph</div>
+                <div className="feels-like">Feels Like: {Math.round(weather.main.feels_like)} &deg;f</div>
+                <div className="humidity">Humidity: {Math.round(weather.main.humidity)}%</div>
+                <div className="sunrise">Sunrise: {moment(weather.sys.sunrise * 1000).format('LT')}</div>
+                <div className="sunset">Sunset: {moment(weather.sys.sunset * 1000).format('LT')}</div>
+              </div>
             </div>
-            <div className="weather">{weather.weather[0].main}</div>
           </div>
-        </div>
-        ) : ('')}
-      </main>
+          ) : ('')}
+        </main>
+      </div>
     </div>
   );
 }
 
 export default App;
+
